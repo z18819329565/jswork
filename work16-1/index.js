3b12c6142a16f7a52081b520e11f56c2f14ecf9f
z18819329565 <1492200346@qq.com>
2019-12-16 09:36:07 +0800

    work16

---
+++ work16-1/index.js
@@ -0,0 +1,35 @@
+function animate(obj,option) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function() {
+        var flag = true;
+        for (var k in option) {
+            var leader = parseInt(getStyle(obj,k)) || 0;
+            var target = option[k];
+            var step = (target - leader) / 10;
+            step = step > 0 ? Math.ceil(step) : Math.floor(step);
+            leader = leader + step;
+            obj.style[k] = leader + 'px';
+            if (leader != target) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            clearInterval(obj.timer);
+        }
+    },15);
+}
+function getStyle(obj,attr) {
+    if (window.getComputedStyle) {
+        return window.getComputedStyle(obj,null)[attr];
+    } else {
+        return obj.currentStyle[attr];
+    }
+}
+var obj = document.getElementById('box');
+console.log(obj);
+obj.onclick = function () {
+    var left = parseInt(Math.random() * 700);
+    var top = parseInt(Math.random() * 700);
+    console.log(left);
+    animate(obj,{'left':left, 'top': top});
+};
